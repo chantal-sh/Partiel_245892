5b3615f5df7f9dfb12bce5373ce157a27893e48b
Chantal2015 <chantalsaadhajjar@gmail.com>
2024-10-23 17:21:00 +0300

    Saisir et affichage d'un vecteur

--- Ex1/ex1.cpp
+++ Ex1/ex1.cpp
@@ -5,7 +5,27 @@
 
 using namespace std;
 
-int main() {
-	vector<int> v;
+void saisir(vector<int>& v, int n) {
+	for (int i = 0; i < n; i++) {
+		int a;
+		cout << "Saisir element " << i + 1 << ":";
+		cin >> v[i];
+		
+	}
+}
 
+void afficher(vector<int>& v) {
+	for (size_t i = 0; i < v.size(); i++) {
+		cout << v[i] << " ";
+	}
+	cout << endl;
+}
+int main() {
+	int n;
+	cout << "Saisir le nb d'elements " ;
+	cin >> n;
+	vector<int> v(n);
+	saisir(v, n);
+	afficher(v);
+	return 0;
 }
