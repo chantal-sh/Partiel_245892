af016040883ae96bed6ec4ea3476627bb8300c60
Chantal2015 <chantalsaadhajjar@gmail.com>
2024-10-23 17:37:07 +0300

    Implementation de find

--- Ex1/ex1.cpp
+++ Ex1/ex1.cpp
@@ -20,12 +20,37 @@ void afficher(vector<int>& v) {
 	}
 	cout << endl;
 }
+
+int find(const vector<int>& v, int elt, int occ = 1) {
+	if (occ != -1) {
+		for (size_t i = 0; i < v.size(); i++) {
+			if (v[i] == elt) {
+				return i;
+			}
+		}
+		return -1; 
+	}
+	else {
+		for (int i = v.size()-1; i >= 0; i--) {
+			if (v[i] == elt) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+}
+	
+
 int main() {
 	int n;
 	cout << "Saisir le nb d'elements " ;
 	cin >> n;
-	vector<int> v(n);
-	saisir(v, n);
+//	vector<int> v(n);
+//	saisir(v, n);
+	vector<int> v{ 2,5,6,5,10 };
 	afficher(v);
+	cout << "Derniere occurence de 5 est à l'indice " << find(v, 5, -1) << endl;
+
 	return 0;
 }
